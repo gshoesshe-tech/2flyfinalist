aedb47e140552aa35f2d98aec3ea5e7d83c9afec
gshoesshe-tech <gshoesshe@gmail.com>
2025-12-30 08:54:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Edit this file (config.js) and fill in your Supabase keys.
 // IMPORTANT: Use the ANON key only (safe for client-side with RLS + RPC).
-window.__SUPABASE_URL__ = "https://hskxqcqvhbhuoghdksru.supabase.co";
-window.__SUPABASE_ANON_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhza3hxY3F2aGJodW9naGRrc3J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwMzQ0NjUsImV4cCI6MjA4MjYxMDQ2NX0.s5NuPKrf51I-uLJDrm_riw_UVl_ZcbHuS2ujeft_Cp8";
+window.__SUPABASE_URL__ = "https://phckuhgjhcmyvquvcsqm.supabase.co";
+window.__SUPABASE_ANON_KEY__ = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBoY2t1aGdqaGNteXZxdXZjc3FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcwNTU4MDEsImV4cCI6MjA4MjYzMTgwMX0.AMPYrFFl47lS5b4eRt_gTNBVGy3wiR2siKCtzRBdf8A";
